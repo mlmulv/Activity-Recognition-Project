83280af1fb673df84de4b9eaabf0fc909e9511b3
Markus Mulvihill <mulv@UG.KTH.SE>
2025-05-20 18:57:34 +0200

    Moved to .ipynb instead of a main.py file. Reformatted the data, thus had to change the preprocess function. I get results with the method in the HMM file, but main .ipynb file needs to be formatted

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.preprocess import preprocess
-
-def main():
-    # Preprocess the data into training and testing
-    X_train, X_test, y_train, y_test = preprocess()
-
-if __name__ == "__main__":
-    main()
